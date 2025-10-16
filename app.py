b37503211af01f9e307bc4fd561439b016c29095
henry jia <henryhjia@gmail.com>
2025-10-16 09:29:10 -0500

    Fix: Pytest and SQLAlchemy warnings resolved.

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ class LoginForm(FlaskForm):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
